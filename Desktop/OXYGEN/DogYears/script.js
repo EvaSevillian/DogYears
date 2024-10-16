72474a3cb54b52e029d1b8bb5480b5da327a7d47
Eva Sevillano Plata <evasevillanoplata@MacBook-Air-de-Eva.local>
2024-10-16 11:10:38 +0200

    Add JS

--- Desktop/OXYGEN/DogYears/script.js
+++ Desktop/OXYGEN/DogYears/script.js
@@ -22,3 +22,4 @@ const myName = 'Eva Sevillano'.toLowerCase();
 
 //creating the console result in a readable way
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
